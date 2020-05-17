b20c52b6e5f392e644fea7aaa9641511f2cc68c2
vadaszbence <64799151+vadaszbence@users.noreply.github.com>
2020-05-17 10:45:09 +0200

    First commit

---
+++ Ideabank.py
@@ -0,0 +1,17 @@
+ideas = []
+for i in range(1):
+    ideas.append(input("What is your new idea?: "))
+f = open('ideas.txt', 'r')
+
+print(f)
+
+
+
+
+#TODO: write if statement to check if the program was called with --list flag
+#TODO: open ideas.txt file
+#TODO: read all lines from ideas.txt file
+#TODO: for ciklus (loop) to print elements of lists
+
+#TODO: how to add file
+#TODO: delete to/from file
